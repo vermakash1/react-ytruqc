0ac0fdadba349a777c507bb16be3c6540389fba7
vermakash1 <akvermapbt99@gmail.com>
2021-06-22 20:22:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ytruqc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ytruqc)
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+  <h1>Akash</h1>
+  <img src="https://drive.google.com/file/d/1QlTp31irQdDXRkUORA5d34wSgn3mO-5D/view?usp=sharing" alt=""/>
+ </div>
+  
+  , document.getElementById("root"));
